aa573e01c3dae84baca974a25ab443f260f05ca4
thaborak <thaborak@gmail.com>
2018-03-06 16:16:52 -0500

    Error-Handling Middleware 404 & BOOM test

--- server.js
+++ server.js
@@ -12,16 +12,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(morgan('combined'));
 
-// Listen for incoming connections
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
 // INSERT EXPRESS APP CODE HERE...
-
-
-
 
 app.get('/api/notes', (req, res) => {
   const searchTerm = req.query.searchTerm;
@@ -36,4 +27,30 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(data.find(item => item.id === Number(id)));
 });
 
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
 
+
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+
+// Listen for incoming connections
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
